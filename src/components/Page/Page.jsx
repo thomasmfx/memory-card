6e02e0051627989561061c27902f52b31cb3f973
thomasmfx <thomasmoisesf@gmail.com>
2024-11-04 21:04:36 -0300

    🌟 feat: update Page to display brawlers cards

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,9 +1,30 @@
-import Footer from './Footer/Footer'
 import './Page.css'
+import Footer from './Footer/Footer'
+import Card from '../Card/Card'
+import { pickRandom, fetchBrawlers } from '../../services/api'
+import { useState, useEffect } from 'react'
 
 export default function Page() {
+  const [brawlers, setBrawlers] = useState([])
+
+  useEffect(() => {
+    fetchBrawlers()
+      .then((data) => setBrawlers(pickRandom(data, 12)))
+  }, [])
+
   return (
     <>
+      <main className='main'>
+        <div className='cards-container'>
+          {brawlers.map((brawler) =>
+            <Card
+              key={brawler.id}
+              brawlerName={brawler.name}
+              brawlerImage={brawler.imageUrl3}
+            />
+          )}
+        </div>
+      </main>
       <Footer/>
     </>
   )
