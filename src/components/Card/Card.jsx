592ecf9d727067a484726e0f6df674ae585849c6
thomasmfx <thomasmoisesf@gmail.com>
2024-11-05 22:08:56 -0300

    🌟 feat: add cards animation

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,16 +2,25 @@ import './Card.css'
 
 export default function Card({onClick, brawlerId, brawlerName, brawlerImage}) {
   return (
-    <div className="card" data-brawler-id={brawlerId} onClick={() => onClick()}>
-      <div className="outer-card">
-      <div className="inner-card">
-        <img 
-          className='card-brawler-image'
-          src={brawlerImage}
-          alt={'brawler' + brawlerName} 
-          />
-          <p className="card-brawler-name">{brawlerName}</p>
-      </div>
+    <div 
+      className="card-container" 
+      data-brawler-id={brawlerId} 
+      onClick={() => onClick()}
+    >
+      <div className="card">
+        <div className="card-front inner-card">
+          <div className="card-front-content card-content">
+            <img 
+              className='card-brawler-image'
+              src={brawlerImage}
+              alt={'brawler' + brawlerName} 
+            />
+            <p className="card-brawler-name">{brawlerName}</p>
+          </div>
+        </div>
+        <div className="card-back inner-card">
+          <div className="chip"></div>
+        </div>
       </div>
     </div>
   )
