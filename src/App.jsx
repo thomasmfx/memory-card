b0dd18d8187952324417ca1d85e81fe8463b7f72
thomasmfx <thomasmoisesf@gmail.com>
2024-11-15 16:46:09 -0300

    🌟 feat: add vercel's SpeedInsights component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Page from './components/Page/Page'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <>
       <Page/>
+      <SpeedInsights/>
     </>
   )
 }
